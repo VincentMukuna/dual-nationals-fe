c6d523b6ad98d49f8769a01a8e3bc2f2d42ba6c6
Vincent Mukuna <mukunavincent28@gmail.com>
2023-12-29 00:29:23 +0300

    ui fix

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex min-h-[100px] w-full resize-none rounded-md  bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50  dark:bg-gray-900 dark:ring-offset-gray-950 dark:placeholder:text-gray-500 dark:focus-visible:ring-primary-500',
+          'flex min-h-[100px] w-full resize-none rounded-md  bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50  dark:bg-gray-900 dark:ring-offset-gray-950 dark:placeholder:text-gray-600 dark:focus-visible:ring-primary-500',
           className
         )}
         ref={ref}
