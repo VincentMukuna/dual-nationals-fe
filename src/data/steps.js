df0c4436a4dea130fc1ac384e64ff7cd73841da0
Vincent Mukuna <mukunavincent28@gmail.com>
2023-12-28 23:42:33 +0300

    [chore] add steps data

---
+++ src/data/steps.js
@@ -0,0 +1,24 @@
+const steps = [
+  {
+    title: 'Choose your plan',
+    image: '/static/images/steps/step-1.png',
+    text: "Whether you're a fan, content creator, current player, or an administrator we have the perfect plan to get you started. ",
+  },
+  {
+    title: 'Create a Profile',
+    image: '/static/images/steps/step-2.png',
+    text: 'Complete a short registration process and begin searching our comprehensive database of players. Whether your looking new prospects for your country or need to update your player profile with your nationality(s), Dual Nationals is the perfect resource for you.',
+  },
+  {
+    title: 'Start searching',
+    image: '/static/images/steps/step-3.png',
+    text: "Whether you're a fan, player, or administrator you can search, save and create collections  of players to share them on social media, with friends, or colleagues who are interested in learning more about  players international eligibility. ",
+  },
+  {
+    title: 'Save and share',
+    image: '/static/images/steps/step-4.png',
+    text: "Whether you're a fan, player, or administrator you can search, save and create collections  of players to share them on social media, with friends, or colleagues who are interested in learning more about  players international eligibility.",
+  },
+]
+
+module.exports = steps
