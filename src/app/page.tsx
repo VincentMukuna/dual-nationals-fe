309b85b93d4e12f3c2df2f01a86db6493b3cb1cb
Vincent Mukuna <mukunavincent28@gmail.com>
2023-12-29 20:57:30 +0300

    [feat] use PPR in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import NewsHighlights from '@/app/(homepage)/NewsHighlights'
 import PlayerSearchBox from '@/app/(homepage)/PlayerSearchBox'
-import PlayerSpotlight from '@/app/(homepage)/PlayerSpotlight'
-import MostViewedPlayers from './(homepage)/MostViewedPlayers'
+import PlayerSpotlight, { PlayerSpotlightSkeleton } from '@/app/(homepage)/PlayerSpotlight'
+import MostViewedPlayers, { MostViewedPlayersSkeleton } from './(homepage)/MostViewedPlayers'
 import NewsFeed from './(homepage)/NewsFeed'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <main className="flex flex-col  gap-8 pt-2 md:max-w-none">
       <NewsHighlights />
       <PlayerSearchBox />
-      <PlayerSpotlight />
-      <MostViewedPlayers />
+      <Suspense fallback={<PlayerSpotlightSkeleton />}>
+        <PlayerSpotlight />
+      </Suspense>
+      <Suspense fallback={<MostViewedPlayersSkeleton />}>
+        <MostViewedPlayers />
+      </Suspense>
+
       <NewsFeed />
     </main>
   )
