fbcab9f6f764bc65618c836d8de8e2bc6250d82b
Vincent Mukuna <mukunavincent28@gmail.com>
2023-12-29 16:30:19 +0300

    Made player card reusable

--- src/app/(homepage)/PlayerCard.tsx
+++ src/app/(homepage)/PlayerCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Avatar, AvatarFallback, AvatarImage } from '../../components/ui/avatar'
 import { Heart, Share2, Tag, User } from 'lucide-react'
-import { getPosInitials } from '@/lib/utils'
+import { cn, getPosInitials } from '@/lib/utils'
 import { Card, CardContent } from '../../components/ui/card'
 import {
   IconBall,
@@ -10,13 +10,37 @@ import {
   IconJersey,
   IconVerified,
 } from '../../components/icons'
-import { useContext, createContext } from 'react'
+import { useContext, createContext, ReactNode } from 'react'
 
-const PlayerContext = createContext<any>({})
+const PlayerCardContext = createContext<any>({})
 
-export default function PlayerCard({ player, variant }: { player: any; variant?: 'summary' }) {
+export function PlayerCard({
+  player,
+  children,
+  className,
+}: {
+  player: any
+  children: ReactNode
+  className?: string
+}) {
   return (
-    <PlayerContext.Provider value={player}>
+    <PlayerCardContext.Provider value={player}>
+      <Card>
+        <CardContent className={className}>{children}</CardContent>
+      </Card>
+    </PlayerCardContext.Provider>
+  )
+}
+
+export default function PlayerCardDefault({
+  player,
+  variant,
+}: {
+  player: any
+  variant?: 'summary'
+}) {
+  return (
+    <PlayerCardContext.Provider value={player}>
       <Card className="ml-2">
         <CardContent
           className="flex flex-col gap-4 divide-y divide-gray-800 p-0 px-3 pb-2 text-sm text-gray-300
@@ -25,60 +49,101 @@ export default function PlayerCard({ player, variant }: { player: any; variant?:
         >
           <div className="flex flex-col gap-1">
             <div className="flex justify-between">
-              <Avatar className="h-14 w-14 rounded-sm">
-                <AvatarImage src={player.headshot} alt={`@${player.name}`} className="" />
-                <AvatarFallback>
-                  <User />
-                </AvatarFallback>
-              </Avatar>
+              <PlayerCardAvatar />
               <div className="flex space-x-2">
                 <Share2 />
                 <Heart />
               </div>
             </div>
-
-            <span className=" flex items-center font-semibold">
-              {player.name}
-              <IconVerified />
-            </span>
+            <PlayerCardName />
             <div className="flex items-center gap-2 text-[0.72rem] sm:text-xs">
-              <div className="flex items-center gap-1 ">
-                <IconJersey />
-                <span>{player['jersey_number'] || 'N/A'}</span>
-              </div>
+              <PlayerCardJerseyNo />
               <span>{` • `}</span>
-              <span>{getPosInitials(player['main_position'])}</span>
+              <PlayerCardPosition />
               <span>{` • `}</span>
-              <span>
-                {' '}
-                <span className="text-gray-400">Age</span> {`${player['age']}`}
-              </span>
+              <PlayerCardAge />
               <span>{` • `}</span>
-              <div className="flex gap-1">
-                <Tag className="h-4 w-4" />
-                {`€${player['market_value'] || 'N/A'}`}
-              </div>
+              <PlayerCardPriceTag />
             </div>
           </div>
-          <PlayerClubStats />
-          <PlayerNTStats />
+          <PlayerCardClubStats />
+          <PlayerCardNTStats />
         </CardContent>
       </Card>
-    </PlayerContext.Provider>
+    </PlayerCardContext.Provider>
   )
 }
 
-function PlayerClubStats() {
-  const player = useContext(PlayerContext)
+export function PlayerCardName({ className }: { className?: string }) {
+  const player = useContext(PlayerCardContext)
+  return (
+    <span className={cn(' flex items-center font-semibold', className)}>
+      {player.name}
+      <IconVerified />
+    </span>
+  )
+}
+
+export function PlayerCardAvatar({ className }: { className?: string }) {
+  const player = useContext(PlayerCardContext)
+  return (
+    <Avatar className={cn('h-14 w-14 rounded-sm', className)}>
+      <AvatarImage src={player.headshot} alt={`@${player.name}`} className="" />
+      <AvatarFallback>
+        <User />
+      </AvatarFallback>
+    </Avatar>
+  )
+}
+export function PlayerCardAge() {
+  const player = useContext(PlayerCardContext)
+  return (
+    <span>
+      <span className="text-gray-400">Age</span> {`${player['age']}`}
+    </span>
+  )
+}
+export function PlayerCardJerseyNo() {
+  const player = useContext(PlayerCardContext)
+  return (
+    <div className="flex items-center gap-1 ">
+      <IconJersey />
+      <span>{player['jersey_number'] || 'N/A'}</span>
+    </div>
+  )
+}
+
+export function PlayerCardPosition() {
+  const player = useContext(PlayerCardContext)
+  return <span>{getPosInitials(player['main_position'])}</span>
+}
+export function PlayerCardPriceTag() {
+  const player = useContext(PlayerCardContext)
+  return (
+    <div className="flex gap-1">
+      <Tag className="h-4 w-4" />
+      {`€${player['market_value'] || 'N/A'}`}
+    </div>
+  )
+}
+
+export function PlayerCardClubLogo({ className }: { className?: string }) {
+  const player = useContext(PlayerCardContext)
+  return (
+    <Avatar className={className}>
+      <AvatarImage src={player.club_logo} />
+      <AvatarFallback />
+    </Avatar>
+  )
+}
+
+export function PlayerCardClubStats() {
+  const player = useContext(PlayerCardContext)
 
   return (
     <div className="flex flex-col gap-2 py-1">
       <div className="flex gap-2">
-        <Avatar>
-          <AvatarImage src={player.club_logo} />
-          <AvatarFallback />
-        </Avatar>
-
+        <PlayerCardClubLogo />
         <div className="space-y-[0.1rem]">
           <div className="text-xs text-gray-500">Current season at</div>
           <div className="font-semibold tracking-wide ">{player.club}</div>
@@ -101,22 +166,31 @@ function PlayerClubStats() {
     </div>
   )
 }
-function PlayerNTStats() {
-  const player = useContext(PlayerContext)
+
+export function PlayerCardNTLogo({ className }: { className?: string }) {
+  const player = useContext(PlayerCardContext)
+  return (
+    <Avatar className={className}>
+      <AvatarImage src={player.national_team_flag} />
+      <AvatarFallback />
+    </Avatar>
+  )
+}
+export function PlayerCardNTStats() {
+  const player = useContext(PlayerCardContext)
 
   return (
     <div className="flex flex-col gap-2 py-1">
       <div className="flex gap-2">
-        <Avatar>
-          <AvatarImage src={player.national_team_flag} />
-          <AvatarFallback />
-        </Avatar>
+        <PlayerCardNTLogo />
 
         <div className="flex flex-col  ">
           <div className="font-semibold tracking-wide ">{player.national_team}</div>
           <div className="flex items-center gap-1 text-xs text-gray-500">
             <div>Also eligible for</div>
-            <div className="flex gap-1">{getEligibleFlags(player) || '-'}</div>
+            <div className="flex gap-1">
+              <PlayerCardEligibleFlags />
+            </div>
           </div>
         </div>
       </div>
@@ -138,10 +212,12 @@ function PlayerNTStats() {
   )
 }
 
-function getEligibleFlags(player: any) {
+export function PlayerCardEligibleFlags() {
+  const player = useContext(PlayerCardContext)
+
   if (typeof player.citizenship_flag === 'string') {
     if (player.citizenship_flag === player.national_team_flag) {
-      return
+      return null
     }
     return (
       <Avatar className="h-6 w-6">
