c51dc45cb41b61c75f96830a5e873b5832d8daa2
Vincent Mukuna <mukunavincent28@gmail.com>
2023-12-31 15:26:27 +0300

    [chore] add semantic html

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -21,14 +21,14 @@ export default function AboutPage() {
       />
       <div className="relative -mt-6 overflow-hidden py-8 opacity-90">
         <div className=" mt-2 flex flex-col  gap-10 md:flex-row">
-          <div className="shrink-0 basis-[40%]  self-start border-s-2  border-green-600 pl-4 text-lg font-semibold tracking-wide ">
+          <p className="shrink-0 basis-[40%]  self-start border-s-2  border-green-600 pl-4 text-lg font-semibold tracking-wide ">
             Lets unravel the mechanics behind Dual Nationals
-          </div>
-          <div className="leading-relaxed  sm:max-w-md  lg:max-w-xl">
+          </p>
+          <p className="leading-relaxed  sm:max-w-md  lg:max-w-xl">
             Our platform is designed to make it easy for fans, players, and agents/administrators to
             quickly access information on players who are eligible to compete for a particular
             country&apos;s national team.
-          </div>
+          </p>
         </div>
       </div>
       <Image
@@ -40,10 +40,10 @@ export default function AboutPage() {
       />
       <div className="relative -mt-6 overflow-hidden py-8 opacity-90">
         <div className=" mt-2 flex flex-col  gap-10 md:flex-row">
-          <div className="shrink-0 basis-[40%]  self-start border-s-2  border-blue-600 pl-4 text-lg font-semibold tracking-wide ">
+          <p className="shrink-0 basis-[40%]  self-start border-s-2  border-blue-600 pl-4 text-lg font-semibold tracking-wide ">
             The most comprehensive database of players
-          </div>
-          <div className="leading-relaxed  sm:max-w-md  lg:max-w-xl">
+          </p>
+          <p className="leading-relaxed  sm:max-w-md  lg:max-w-xl">
             At Dual Nationals, we understand the importance of having accurate and up-to-date
             information when it comes to national team selection. Our team of experts is committed
             to providing you with the most comprehensive database of eligible players from around
@@ -51,7 +51,7 @@ export default function AboutPage() {
             prospects, a player seeking to represent your country of origin, or an
             agent/administrator searching for new talent, Dual Nationals is the perfect resource for
             you.
-          </div>
+          </p>
         </div>
       </div>
       <Image
@@ -63,17 +63,17 @@ export default function AboutPage() {
       />
       <div className="relative -mt-6 overflow-hidden py-8 opacity-90">
         <div className=" mt-2 flex flex-col  gap-10 md:flex-row">
-          <div className="shrink-0 basis-[40%]  self-start border-s-2  border-blue-600 pl-4 text-lg font-semibold tracking-wide ">
+          <p className="shrink-0 basis-[40%]  self-start border-s-2  border-blue-600 pl-4 text-lg font-semibold tracking-wide ">
             Finding the information you need has never been easier
-          </div>
-          <div className="leading-relaxed sm:max-w-md  lg:max-w-xl">
+          </p>
+          <p className="leading-relaxed sm:max-w-md  lg:max-w-xl">
             Our database is constantly updated with new players and information, so you can be sure
             that you&apos;re always getting the most current data available. At Dual Nationals,
             we&apos;re passionate about the beautiful game and the role that national teams play in
             bringing people together from all walks of life. We believe that everyone should have
             the opportunity to represent their country of origin, and we&apos;re proud to provide a
             platform that helps make that dream a reality.
-          </div>
+          </p>
         </div>
       </div>
     </div>
