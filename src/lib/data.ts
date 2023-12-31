61215af685e3e430da45b5da5d43db63938de39c
Vincent Mukuna <mukunavincent28@gmail.com>
2023-12-31 15:32:11 +0300

    [fix] dev data fetching

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,13 +5,13 @@ import { z } from 'zod'
 const PlayersArraySchema = z.array(PlayerSchema)
 
 export async function fetchPlayers(query: string = '') {
-  const url = process.env['NEXT_PUBLIC_BACKEND_URL']
+  const url = process.env['BACKEND_URL']
   const res = await fetch(`${url}/players?${query}`)
   return await res.json()
 }
 
 export async function fetchPlayer(id: string) {
-  const url = process.env['NEXT_PUBLIC_BACKEND_URL']
+  const url = process.env['BACKEND_URL']
   const res = await fetch(`${url}/players?id=${id}`)
   return await res.json()
 }
