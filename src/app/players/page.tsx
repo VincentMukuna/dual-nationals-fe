865f3a24dd51f99086025328a17806dc1b205777
Vincent Mukuna <mukunavincent28@gmail.com>
2024-01-05 23:17:41 +0300

    [feat] add player search

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -1,5 +1,17 @@
-import React from 'react'
+import SearchInput from '@/components/search-input'
+import PlayerSearchResultsList from '@/components/search-results'
+import { Input } from '@/components/ui/input'
+import { Suspense } from 'react'
 
-export default function PlayersPage() {
-  return <div>PlayersPage</div>
+export default function SearchPlayers({ searchParams }: { searchParams: { search: string } }) {
+  return (
+    <main>
+      <div className="mt-8 flex flex-col gap-4">
+        <SearchInput />
+        <Suspense fallback={<p>Loading...</p>}>
+          <PlayerSearchResultsList query={searchParams.search} />
+        </Suspense>
+      </div>
+    </main>
+  )
 }
