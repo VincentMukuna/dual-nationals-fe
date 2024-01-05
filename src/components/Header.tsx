f66922c24548cb479c67fb72a525e88072bf0e5e
Vincent Mukuna <mukunavincent28@gmail.com>
2024-01-05 23:18:03 +0300

    [feat] add login link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,8 @@ import MobileNav from './MobileNav'
 import SearchButton from './SearchButton'
 import { usePathname } from 'next/navigation'
 import Logo from './icons/logo'
+import { User } from 'lucide-react'
+import CustomLink from './Link'
 
 const Header = () => {
   const pathname = usePathname()
@@ -16,7 +18,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center gap-2 ">
             <Logo aria-label="App logo" />
-            <div className="block">
+            <div className="hidden sm:block">
               <p className="text-sm font-semibold uppercase">{siteMetadata.title}</p>
               <p className="hidden text-xs uppercase text-gray-300 sm:block">
                 {siteMetadata.strapline}
@@ -39,28 +41,12 @@ const Header = () => {
             {link.title}
           </Link>
         ))}
-        <div className="ml-4 flex gap-2">
-          <SearchButton />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 36 36"
-            className="h-7 w-7"
-          >
-            <path
-              fill="currentColor"
-              d="M18 17a7 7 0 1 0-7-7a7 7 0 0 0 7 7m0-12a5 5 0 1 1-5 5a5 5 0 0 1 5-5"
-              className="clr-i-outline clr-i-outline-path-1"
-            />
-            <path
-              fill="currentColor"
-              d="M30.47 24.37a17.16 17.16 0 0 0-24.93 0A2 2 0 0 0 5 25.74V31a2 2 0 0 0 2 2h22a2 2 0 0 0 2-2v-5.26a2 2 0 0 0-.53-1.37M29 31H7v-5.27a15.17 15.17 0 0 1 22 0Z"
-              className="clr-i-outline clr-i-outline-path-2"
-            />
-            <path fill="none" d="M0 0h36v36H0z" />
-          </svg>
-        </div>
+      </div>
+      <div className="ml-4 flex items-center gap-2">
+        <SearchButton />
+        <CustomLink href={'/login'}>
+          <User />
+        </CustomLink>
       </div>
     </header>
   )
