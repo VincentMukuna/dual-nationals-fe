063fd2837a8e2c4020df6b0514e052ff170d8266
Vincent Mukuna <mukunavincent28@gmail.com>
2024-01-09 03:30:32 +0300

    [feat] add a tag filter

---
+++ src/components/search/filters/tag-filter.tsx
@@ -0,0 +1,59 @@
+'use client'
+import { Tag, TagInput } from '@/components/tag/tag-input'
+import { Label } from '@/components/ui/label'
+import useURLParam from '@/lib/hooks/useURLParam'
+import _ from 'lodash'
+import { useEffect, useState } from 'react'
+
+export default function TagFilter({ param }: { param: string }) {
+  const { handleChange, searchParams, pathname, router } = useURLParam()
+
+  function getInitialParams() {
+    let initialTags: Tag[] = []
+    searchParams.forEach((val, key) => {
+      if (key === param) {
+        initialTags.push({ id: _.uniqueId(), text: val })
+      }
+    })
+    return initialTags
+  }
+
+  const [tags, setTags] = useState<Tag[]>(getInitialParams())
+
+  function handleTagChange(value: Tag[]) {
+    const params = new URLSearchParams(searchParams)
+    params.delete(param)
+    const query: string[] = []
+    for (const tag of value) {
+      query.push(`${param}=${tag.text}`)
+    }
+    const queryStr = query.join('&')
+
+    router.replace(`${pathname}?${params.toString()}&${queryStr}`)
+  }
+
+  useEffect(() => {
+    handleTagChange(tags)
+  }, [tags])
+
+  return (
+    <div className="grid gap-2">
+      <Label>Nationality</Label>
+      <div className="rounded-md bg-gray-900 p-3">
+        <TagInput
+          size={'sm'}
+          tags={tags}
+          setTags={(newTags) => {
+            console.log('tags change', newTags)
+            setTags(newTags)
+          }}
+          maxTags={4}
+          placeholder="Add a nationality"
+          placeholderWhenFull="Maximum of 4 nationalities allowed"
+          draggable
+          className=" p-2 focus-visible:ring-0 focus-visible:ring-offset-0"
+        />
+      </div>
+    </div>
+  )
+}
