65ee77e01a81a6f3fef78118c047829d53985a1c
Vincent Mukuna <mukunavincent28@gmail.com>
2024-01-09 03:26:25 +0300

    [feat] add mobile flters

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -1,17 +1,28 @@
-import SearchInput from '@/components/search-input'
-import PlayerSearchResultsList from '@/components/search-results'
+import SearchFilters from '@/components/search/filters/search-filters'
+import SearchInput from '@/components/search/search-input'
+import PlayerSearchResultsList from '@/components/search/search-results'
 import { Input } from '@/components/ui/input'
 import { Suspense } from 'react'
 
-export default function SearchPlayers({ searchParams }: { searchParams: { search: string } }) {
+export default function SearchPlayers({
+  searchParams,
+}: {
+  searchParams: { [index: string]: string | number }
+}) {
+  console.log(searchParams)
   return (
-    <main>
-      <div className="mt-8 flex flex-col gap-4">
-        <SearchInput />
-        <Suspense fallback={<p>Loading...</p>}>
-          <PlayerSearchResultsList query={searchParams.search} />
-        </Suspense>
-      </div>
-    </main>
+    <div className="mt-4 grid gap-4 ">
+      <SearchInput />
+      <main className="grid gap-6 divide-gray-800 xl:grid-cols-12 xl:divide-x">
+        <aside className="col-span-3 hidden xl:block">
+          <SearchFilters />
+        </aside>
+        <div className="col-span-9 flex flex-col gap-4 xl:pl-6">
+          <Suspense fallback={<p>Loading...</p>}>
+            <PlayerSearchResultsList searchParams={searchParams} />
+          </Suspense>
+        </div>
+      </main>
+    </div>
   )
 }
