873423075917bb22e97817e7f41ff424a7f30c23
Vincent Mukuna <mukunavincent28@gmail.com>
2024-01-13 04:04:04 +0300

    [refactor] filter values

--- src/components/search/filters/search-filters.tsx
+++ src/components/search/filters/search-filters.tsx
@@ -6,13 +6,13 @@ import TagFilter from './tag-filter'
 
 export default function SearchFilters() {
   return (
-    <div className="grid gap-y-4 ">
+    <div className="grid gap-y-4">
       <div className="flex flex-wrap  items-baseline justify-between gap-x-6 ">
         <span className="">Filter your results</span>
 
         <ClearFilters />
       </div>
-      <div className="grid gap-8">
+      <div className="grid gap-4">
         <StringFilter label="Player name" param="name_like" />
         <StringFilter label="Club" param="club_like" />
         <RangeFilter label="Age" param="age" units="years" min={12} max={50} defaults={[20, 35]} />
@@ -24,24 +24,6 @@ export default function SearchFilters() {
           min={140}
           max={300}
           defaults={[170, 240]}
-        />
-        <RadioFilter
-          label="Kicking Foot"
-          param="position_like"
-          options={[
-            {
-              label: 'Both',
-              value: 'Both',
-            },
-            {
-              label: 'Left',
-              value: 'Left',
-            },
-            {
-              label: 'Right',
-              value: 'Right',
-            },
-          ]}
         />
 
         <RangeFilter
@@ -56,51 +38,51 @@ export default function SearchFilters() {
           <span>Position(s)</span>
           <RadioFilter
             label="Defensive"
-            param="position_like"
+            param="main_position_like"
             options={[
               {
                 label: 'GK',
-                value: 'GoalKeeper',
+                value: 'Goalkeeper',
               },
               {
                 label: 'CB',
-                value: 'Center Back',
+                value: 'Centre-Back',
               },
               {
                 label: 'RB',
-                value: 'Right Back',
+                value: 'Right-Back',
               },
             ]}
           />
           <RadioFilter
             label="Midfielder"
-            param="position_like"
+            param="main_position_like"
             options={[
               {
                 label: 'CM',
-                value: 'Center Midfield',
+                value: 'Central Midfield',
               },
               {
                 label: 'CAM',
-                value: 'Central Attacking Midfielder',
+                value: 'Attacking Midfield',
               },
               {
                 label: 'DM',
-                value: 'Defensive Midfielder',
+                value: 'Defensive Midfield',
               },
               {
                 label: 'RM',
-                value: 'Right Midfielder',
+                value: 'Right Midfield',
               },
               {
                 label: 'LM',
-                value: 'Left Midfielder',
+                value: 'Left Midfield',
               },
             ]}
           />
           <RadioFilter
             label="Attacker"
-            param="position_like"
+            param="main_position_like"
             options={[
               {
                 label: 'LW',
@@ -108,7 +90,7 @@ export default function SearchFilters() {
               },
               {
                 label: 'RW',
-                value: 'Right Wing',
+                value: 'Right Winger',
               },
               {
                 label: 'ST',
